4e0c32b9d0ddab3193850b92eb83ad92b3272159
Wolfysteyr <looptyfloops@gmail.com>
2024-03-24 21:40:52 +0200

    test

---
+++ src/main/java/com/example/makdan_project/LoginController.java
@@ -0,0 +1,40 @@
+package com.example.makdan_project;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private PasswordField passField;
+
+    @FXML
+    private Button signupButton;
+
+    @FXML
+    private TextField userField;
+
+    @FXML
+    private Label wrongPass;
+
+    @FXML
+    private Label wrongUser;
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signup(ActionEvent event) {
+
+    }
+
+}
